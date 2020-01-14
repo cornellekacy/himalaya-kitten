97343305420a0b9f8088ca449407d0ac65a326ed
cornellekacy <cornellekacy4@gmail.com>
2020-01-14 07:35:58 +0400

    2

---
+++ send.php
@@ -0,0 +1,68 @@
+    <?php
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+// Load Composer's autoloader
+require 'autoload.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+if (array_key_exists('email', $_POST)) {
+    date_default_timezone_set('Etc/UTC');
+    require 'autoload.php';
+    //Create a new PHPMailer instance
+    $mail = new PHPMailer;
+    //Tell PHPMailer to use SMTP - requires a local mail server
+    //Faster and safer than using mail()
+    $mail->isSMTP();
+     // $mail->SMTPDebug = SMTP::DEBUG_SERVER; 
+$mail->SMTPSecure = 'tls';
+$mail->Host = 'smtp.yandex.com';
+$mail->Port = 587;
+//Whether to use SMTP authentication
+$mail->SMTPAuth = true;
+//Username to use for SMTP authentication - use full email address for gmail
+$mail->Username = "contact@petflyrelocation.com";
+//Password to use for SMTP authentication
+$mail->Password = "petflyrelocation45";
+    //Use a fixed address in your own domain as the from address
+    //**DO NOT** use the submitter's address here as it will be forgery
+    //and will cause your messages to fail SPF checks
+    $mail->setFrom('contact@petflyrelocation.com', $_POST['name']);
+    //Send the message to yourself, or whoever should receive contact for submissions
+    $mail->addAddress('contact@petflyrelocation.com', 'Contact Nina Himalayan Cattery');
+    //Put the submitter's address in a reply-to header
+    //This will fail if the address provided is invalid,
+    //in which case we should ignore the whole request
+    if ($mail->addReplyTo($_POST['email'], $_POST['name'])) {
+        $mail->Subject = 'Nina Himalayan Cattery Kittens';
+        //Keep it simple - don't use HTML
+        $mail->isHTML(false);
+        //Build a simple message body
+        $mail->Body = <<<EOT
+Name: {$_POST['name']}
+Email: {$_POST['email']}
+Phone Number: {$_POST['phone']}
+City/State: {$_POST['city']}
+Subject: {$_POST['subject']}
+Message: {$_POST['message']}
+EOT;
+        //Send the message, check for errors
+        if (!$mail->send()) {
+            //The reason for failing to send will be in $mail->ErrorInfo
+            //but you shouldn't display errors to users - process the error, log it on your server.
+            $msg = 'Sorry, something went wrong. Please try again later.'. $mail->ErrorInfo;
+        } else {
+            echo "<script>alert('Message Successfully Sent we will get back to you shortly');
+            window.location.href = 'contact2.html'</script>";
+        }
+    } else {
+        $msg = 'Invalid email address, message ignored.';
+    }
+}
+
+?>
